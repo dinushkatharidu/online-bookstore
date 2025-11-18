48d9e90968d6782af73feb31f7fe72dd135b7934
ASDT000 <asdinushkatharidu@gmail.com>
2025-11-18 22:55:45 +0530

    Setup MongoDB database connection

---
+++ backend/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/database');
+
+// load environment variable 
+dotenv.config();
+
+// Connect to database
+connectDB();
+
+// initialize express app
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// Serve uploaded files
+app.use('/uploads', express.static('uploads'));
+
+// Basic test route
+app.get('/',(req,res) => {
+    res.json({
+        messege: 'Online Bookstore API is running!',
+        version: '1.0.0',
+        endpoints: {
+            auth: '/api/auth',
+            books: 'api/books',
+            cart:   'api/cart',
+            orders: 'api/orders',
+            users: 'api/users'
+        }
+    });
+});
+
+// port
+const PORT = process.env.PORT || 5000;
+
+// start server 
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+    console.log(`Environment: ${process.env.NODE_ENV}`); 
+});
